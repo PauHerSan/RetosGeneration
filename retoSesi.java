e138116a16905cd45763d89042410f223555b79c
Paulina Herrera <83611048+PauHerSan@users.noreply.github.com>
2024-11-12 16:29:37 -0600

    Reto seis de verificación de código
    1. main
    2. new
    3. for, else

---
+++ retoSesi.java
@@ -0,0 +1,35 @@
+package com.generation.revisionCuatro;
+
+import java.util.Scanner;
+
+public class retoSesi {
+
+	public static void main(String[] args) { 
+		
+		int[] n = new int[20]; 
+	for (int i = 0; i < 20; i++) { 
+		n[i] = (int)(Math.random() * 381) + 20; 
+		System.out.print(n[i] + " "); 
+		} 
+	System.out.println("\n¿Qué números quiere resaltar? "); 
+	System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): "); 
+	Scanner scanner = new Scanner(System.in); 
+	int opcion = Integer.parseInt(scanner.nextLine()); 
+	int multiplo = (opcion == 1) ? 5 : 7; 
+	for (int e : n) { 
+		if (e % multiplo == 0) { 
+			System.out.print("[" + e + "] "); 
+			} else { 
+				System.out.print(e + " "); 
+			} 
+		}
+	}
+		
+
+		
+		
+		
+		
+	
+
+}//class
